780d35c11ad33c33386d5a67e990a64a239e12fd
hari shankar bisen <bisenharishankar@gmail.com>
2019-01-24 11:15:01 +0530

    Updates

---
+++ precache-manifest.df62efcb2586f90224fc5656da11ec63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a41c2e97fcaef53006b0",
+    "url": "/react-gh-pages/static/css/main.67606243.chunk.css"
+  },
+  {
+    "revision": "a41c2e97fcaef53006b0",
+    "url": "/react-gh-pages/static/js/main.a41c2e97.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-gh-pages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "148abb5beb4f13a3f96bf2b62d30278d",
+    "url": "/react-gh-pages/index.html"
+  }
+];
